3b962991236ecfa4a027d44ee5450bada8361da6
luis-tavico <3004359830101@ingenieria.usac.edu.gt>
2023-10-25 14:54:40 -0600

    cambios en reports

--- frontend/src/app/components/reports/reports.component.ts
+++ frontend/src/app/components/reports/reports.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ReportsService } from 'src/app/services/reports.service';
-
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-reports',
@@ -9,6 +9,17 @@ import { ReportsService } from 'src/app/services/reports.service';
 })
 export class ReportsComponent {
 
-  constructor(private service: ReportsService) { }
+  files: any[] = [{file: "", name:"reporte1.txt", type:"txt"}, {file:"", name:"reporte2.txt", type:"txt"}];
+  url = "https://quickchart.io/graphviz?graph=digraph G { A -> B; }";
+
+  constructor(private service: ReportsService, private sanitizer: DomSanitizer) { }
+
+  item_selected(i:number) {
+    console.log(i);
+  }
+
+  getSafeUrl() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
 
 }
